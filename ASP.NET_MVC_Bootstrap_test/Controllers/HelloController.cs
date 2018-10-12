3af1ee6c332c1011945144447fd9830ee1df48f5
y0sh-CompanyMyPC-Admin-Win10 <stad.y0sh@gmail.com>
2018-10-12 18:17:06 +0900

    プロジェクト ファイルを追加します。

---
+++ ASP.NET_MVC_Bootstrap_test/Controllers/HelloController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Bootstrap_test.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index(string id)
+        {
+            ViewData["msg"] = "こんにちは";
+            ViewData["id"] = string.Format("id値：{0}", id);
+
+            return View();
+        }
+
+        // 入力フォーム生成
+        public ActionResult Post()
+        {
+            return View();
+        }
+
+        // 送信ボタンクリック時のメソッド
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Post(string name)
+        {
+            ViewData["msg"] = $"こんにちは、{name}さん";
+            return View("Result");
+        }
+
+    }
+}
---
+++ ASP.NET_MVC_Bootstrap_test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC_Bootstrap_test.MvcApplication" Language="C#" %>
